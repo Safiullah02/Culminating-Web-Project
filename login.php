73102faaca1756e1441df4ab8b7007804d01544f
greeeny310 <45203921+greeeny310@users.noreply.github.com>
2018-11-20 09:42:05 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project</title>
+</head>
+<body>
+    <center>
+
+Username:
+<input type="text" name="user" placeholder="Enter Username" required>
+<br>
+Password:
+<input type="Password" name="pass" placeholder="Enter Password" required>
+<br>
+<button type="submit" name="login" value="submit"> login </button>
+<input type="button" onclick="location.href='signup.php';" value="signup" />
+</center>
+
+</body>
+</html>
